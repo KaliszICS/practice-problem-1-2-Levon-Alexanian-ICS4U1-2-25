4322c1c4adb04c30de901b5d2cb811f7a8c67332
Levon-Alexanian-ICS4U1-2-25 <levon.alexanian@student.tdsb.on.ca>
2025-03-27 05:14:34 +0000

    update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,8 +1,32 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static boolean validIndex(int[] array, int index) {
+        try {
+            if (index >= 0 && index < array.length) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+    }
 
-	}
+	public static int divide(int a, int b) {
+        try {
+            return a / b;
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+
+	public static int safeConvertStringtoInt(String str) {
+        try {
+            return Integer.parseInt(str);
+        } catch (Exception e) {
+            return 0;
+        }
+    }
 
 	
 
